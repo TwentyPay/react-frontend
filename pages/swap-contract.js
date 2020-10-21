a0d9f6332462e7351d79e24aebc12c23b3e843cb
John Sanford <jsanford9292@gmail.com>
2020-10-21 05:08:59 -0700

    ERC-20 to ERC-20 payments working now

--- pages/swap-contract.js
+++ pages/swap-contract.js
@@ -11,9 +11,102 @@ function SwapContract() {
     const API_QUOTE_URL = 'https://api.0x.org/swap/v1/quote';
     // Need to hardcode this abi file in the frontend after contract changes
     const { abi: ABI } = require('../src/abi.json');
+    const erc20ABI = require('../src/erc20ABI.json');
+    const tokenAddressUNI = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
+    const tokenHolderUNI = '0x69C5888Ecd21287FBdac5a43D1558Bf73c51E38B';
     const newArgs = {
         deployedAddress: '0x5721931aa166C5d3631a7715F9bE6BE3AE729333',
-        sellAmount: '0.1'
+        roughSellAmount: '1',
+        buyAmount: '5',
+        setupRoughSellAmount: '2',
+        setupBuyAmount: '100'
+    }
+
+    async function setup() {
+        try {
+            await getERC20Token(newArgs);
+            // process.exit(0);
+        } catch (err) {
+            console.error(err);
+            // process.exit(1);
+        }
+    }
+
+    async function getERC20Token(newArgs) {
+        const web3 = createWeb3()
+        const contract = new web3.eth.Contract(ABI, newArgs.deployedAddress);
+        console.log("0x contract assigned")
+
+        // Checking our ETH account balance
+        const [owner] = await web3.eth.getAccounts();
+        console.log("owner account is " + owner)
+        let ownerBal = weiToEther(parseInt(await web3.eth.getBalance(owner)));
+        console.log("owner ETH account balance is " + ownerBal)
+
+        // Convert setupRoughSellAmount and setupBuyAmount from token units to wei.
+        const setupRoughSellAmountWei = etherToWei(newArgs.setupRoughSellAmount);
+        const setupBuyAmountWei = etherToWei(newArgs.setupBuyAmount);
+        console.log("setupRoughSellAmountWei is " + setupRoughSellAmountWei);
+        console.log("setupBuyAmountWei is " + setupBuyAmountWei);
+
+        // Deposit some WETH into the contract. This function accepts ETH and
+        // wraps it to WETH on the fly.
+        console.log(`Depositing ${newArgs.setupRoughSellAmount} ETH (WETH) into the contract at ${newArgs.deployedAddress}...`);
+        await waitForTxSuccess(contract.methods.depositETH().send({
+            value: setupRoughSellAmountWei,
+            from: owner,
+        }));
+
+        // Get a quote from 0x-API to sell the WETH we just deposited into the contract.
+        console.log(`Fetching swap quote from 0x-API to buy ${newArgs.setupBuyAmount} UNI for WETH...`);
+        const qs = createQueryString({
+            sellToken: 'WETH',
+            buyToken: 'UNI',
+            buyAmount: setupBuyAmountWei,
+        });
+        const quoteUrl = `${PROXY_URL + API_QUOTE_URL}?${qs}`;
+        console.info(`Fetching quote ${quoteUrl}...`);
+        const response = await fetch(quoteUrl);
+        const quote = await response.json();
+        console.info(`Received a quote with price ${quote.price}`);
+
+        // Have the contract fill the quote, selling its own WETH.
+        console.info(`Filling the quote through the contract at ${newArgs.deployedAddress}...`);
+        const receipt = await waitForTxSuccess(contract.methods.fillQuote(
+                quote.sellTokenAddress,
+                quote.buyTokenAddress,
+                quote.allowanceTarget,
+                quote.to,
+                quote.data,
+            ).send({
+                from: owner,
+                value: quote.value,
+                gasPrice: quote.gasPrice,
+            }));
+        // console.log('receipt is ' + JSON.stringify(receipt.events))
+        const boughtAmount = weiToEther(receipt.events.SwappedTokens.returnValues.boughtAmount);
+        const soldAmount = weiToEther(receipt.events.SwappedTokens.returnValues.soldAmount);
+        console.info(`${'✔'.bold.green} Successfully sold ${soldAmount} WETH for ${boughtAmount} UNI!`);
+        console.log(`We asked for ${newArgs.setupBuyAmount} tokens originally`)
+
+        // Get UNI balance in contract
+        const contractUNI = new web3.eth.Contract(erc20ABI, tokenAddressUNI);
+        contractUNI.methods.balanceOf(newArgs.deployedAddress).call((error, balance) => {
+                console.log(`Contract now has ${balance.toString()} UNI`);
+        });
+        console.log(`We are asking for ${newArgs.setupBuyAmount} UNI`)
+
+        await waitForTxSuccess(contract.methods.withdrawToken(
+            quote.buyTokenAddress,
+            setupBuyAmountWei,
+        ).send({
+            from: owner,
+        }));
+
+        // Check UNI balance of our account
+        contractUNI.methods.balanceOf(owner).call((error, balance) => {
+                console.log(`Owner address now has ${balance.toString()} UNI`);
+        });
     }
 
     async function awaitRun() {
@@ -27,37 +120,43 @@ function SwapContract() {
     }
 
     async function run(newArgs) {
-        const web3 = createWeb3();
-        console.log("web3 instance created and the sellAmount is " + newArgs.sellAmount)
+        const web3 = createWeb3()
         const contract = new web3.eth.Contract(ABI, newArgs.deployedAddress);
-        console.log("contract assigned!!")
-        const [owner] = await web3.eth.getAccounts();
-        console.log("accounts retrieved and assigned to owner!")
+        console.log("0x contract assigned")
 
-        // Convert sellAmount from token units to wei.
-        const sellAmountWei = etherToWei(newArgs.sellAmount);
-        console.log("sellAmountWei assigned")
+        const [owner] = await web3.eth.getAccounts();
+        console.log("owner account is " + owner)
+
+        // Convert roughSellAmount and buyAmount from token units to wei.
+        const roughSellAmountWei = etherToWei(newArgs.roughSellAmount);
+        const buyAmountWei = etherToWei(newArgs.buyAmount);
+        console.log("roughSellAmountWei is " + roughSellAmountWei);
+        console.log("buyAmountWei is " + buyAmountWei);
 
         // Deposit some WETH into the contract. This function accepts ETH and
         // wraps it to WETH on the fly.
-        console.log(`Depositing ${newArgs.sellAmount} ETH (WETH) into the contract at ${newArgs.deployedAddress}...`);
+        console.log(`Depositing ${newArgs.roughSellAmount} ETH (WETH) into the contract at ${newArgs.deployedAddress}...`);
         await waitForTxSuccess(contract.methods.depositETH().send({
-            value: sellAmountWei,
+            value: roughSellAmountWei,
             from: owner,
         }));
 
-        // Get a quote from 0x-API to sell the WETH we just deposited into the contract.
-        console.log(`Fetching swap quote from 0x-API to sell ${newArgs.sellAmount} WETH for DAI...`);
+        // Get a quote from 0x-API to sell the UNI we just deposited into the contract.
+        console.log(`Fetching swap quote from 0x-API to buy ${newArgs.buyAmount} DAI for UNI...`);
         const qs = createQueryString({
-            sellToken: 'WETH',
+            sellToken: 'UNI',
             buyToken: 'DAI',
-            sellAmount: sellAmountWei,
+            buyAmount: buyAmountWei,
         });
         const quoteUrl = `${PROXY_URL + API_QUOTE_URL}?${qs}`;
         console.info(`Fetching quote ${quoteUrl.bold}...`);
         const response = await fetch(quoteUrl);
         const quote = await response.json();
         console.info(`Received a quote with price ${quote.price}`);
+
+        const contractUNI = new web3.eth.Contract(erc20ABI, tokenAddressUNI);
+        await waitForTxSuccess(contractUNI.methods.approve(quote.allowanceTarget, quote.sellAmount)
+            .send({from: owner}));
 
         // Have the contract fill the quote, selling its own WETH.
         console.info(`Filling the quote through the contract at ${newArgs.deployedAddress.bold}...`);
@@ -72,19 +171,30 @@ function SwapContract() {
                 value: quote.value,
                 gasPrice: quote.gasPrice,
             }));
-        const boughtAmount = weiToEther(receipt.events.BoughtTokens.returnValues.boughtAmount);
-        console.info(`${'✔'.bold.green} Successfully sold ${newArgs.sellAmount.toString().bold} WETH for ${boughtAmount.bold.green} DAI!`);
-        // The contract now has `boughtAmount` of DAI!
+        // console.log('receipt is ' + JSON.stringify(receipt.events))
+        const boughtAmount = weiToEther(receipt.events.SwappedTokens.returnValues.boughtAmount);
+        const soldAmount = weiToEther(receipt.events.SwappedTokens.returnValues.soldAmount);
+        console.info(`${'✔'.bold.green} Successfully sold ${soldAmount} UNI for ${boughtAmount} DAI!`);
+        console.log(`We asked for ${newArgs.buyAmount} tokens originally`)
+        // console.log("Success!!!")
     }
 
     return (
-        <Button
-            color="secondary"
-            className="test0x"
-            onClick={awaitRun}
-        >
-            Test 0x
-        </Button>
+        <div>
+            <Button
+                color="secondary"
+                className="setup"
+                onClick={setup}
+            >
+                Setup
+            </Button>
+            <Button
+                className="test0x"
+                onClick={awaitRun}
+            >
+                Test 0x
+            </Button>
+        </div>
     )
 
 }
